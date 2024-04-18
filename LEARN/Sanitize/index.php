e03c055811172faf983b61ff292a81156ff733cc
JaniBT <janoscsordas072@gmail.com>
2024-04-18 20:28:53 +0200

    learning progress

---
+++ LEARN/Sanitize/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        username: <br>
+        <input type="text" name="username"> <br>
+        age: <br>
+        <input type="text" name="age"> <br>
+        email: <br>
+        <input type="text" name="email"> <br>
+        <input type="submit" name="login" value="login">
+    </form>
+</body>
+</html>
+<?php
+    if (isset($_POST["login"])) {
+
+        // SANITIZE:
+
+        // $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS); // -- Filtereli a speciális karaktereket, hogy ne lehessen malicious kódot elküldeni a szerver felé. Lásd: MySQL script pl.
+        // Side Notes: ``
+        // Példa: filter_input(`Ha GET a method: INPUT_GET, ha POST, akkor: INPUT_POST`, `A name tulajdonsága az input mezőnek`, `És hogy mi alapján akarja filterelni. Lásd: NUMBER_INT for Integers, SPECIAL_CHARS for strings.`)
+
+        // $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+
+        // $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+        // echo "You are $age years old, and your name is: $username! Your email address is: $email";
+
+
+        // VALIDATE:
+
+        $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT); // validátor, ha rendes int-et kap akkor azt adja vissza, ha nem, ez esetben egy üres string-et.
+
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+
+        if (empty($email)) { // Megnézi, hogy üres string e az $age változó
+            echo "That email wasn't valid";
+        } else {
+            echo "Your email is: $email";
+        }
+        
+    }
+?>
---
+++ LEARN/StringFunctions/index.php
@@ -0,0 +1,24 @@
+<?php
+    $username = "Jani The BT";
+    $phone = "123-456-7890";
+
+    // $username = strtolower($username); -- LowerCase
+    // $username = strtoupper($username); -- UpperCase
+    // $username = trim($username);
+    // $username = str_pad($username, 20, "0"); -- Kiegészíteni a stringet valamennyi karakterig valamilyen karakterrel
+    // $phone = str_replace("-", "", $phone);
+    // $username = strrev($username); -- string visszafele
+    // $username = str_shuffle($username); -- Össze keveri a string karaktereit
+    // $equals = strcmp($username, "Jani BT"); -- String összehasonlító
+    // $count = strlen($username); -- string hosszúság
+    // $index = strpos($username, " "); -- string-ben karakter keresés
+    // $firstname = substr($username, 0, 4); -- string-ből karakterek kiszedése megadott indextől indexig.
+    // $fullname = explode(" ", $username); -- string-ből tömb készítése egy megadott szeparátorral.
+
+    // $username = implode("-", $username); -- Tömbböl egy darab string készítése megadott szeparátorral, a különböző értékek között
+
+    // foreach ($fullname as $name) {
+    //     var_dump($name);
+    // } 
+    // echo $fullname;
+?>
